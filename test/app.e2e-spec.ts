0ad4963635662ff7846b6be9822358c19112e1cb
obkin <kapellwork@gmail.com>
2023-10-03 14:26:41 +0300

    added: e2e for GET - success

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -4,6 +4,8 @@ import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
 import { UserUrlDto } from './../src/shrinker/dto/user-url.dto';
 
+const urlHash = 'zw1ous';
+
 const userUrl: UserUrlDto = {
   fullUrl: 'https://youtube.com',
 };
@@ -24,11 +26,11 @@ describe('AppController (e2e)', () => {
     return request(app.getHttpServer()).post('/shrinker').send(userUrl).expect(201);
   });
 
-  it('/shrinker (POST) - fail (url )', () => {
-    return request(app.getHttpServer()).post('/shrinker').send(userUrl).expect(201);
+  it('/shrinker/:hash (GET) - success', () => {
+    return request(app.getHttpServer()).get(`/shrinker/:${urlHash}`).expect(301);
   });
 
-  afterAll(async () => {
+  afterEach(async () => {
     await app.close();
   });
 });
