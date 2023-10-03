31bcc368530c99fac8dac3fd85e3d2661867c37b
obkin <kapellwork@gmail.com>
2023-10-03 16:45:38 +0300

    fixed: e2e test

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -4,11 +4,11 @@ import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
 import { UserUrlDto } from './../src/shrinker/dto/user-url.dto';
 
-const urlHash = 'zw1ous';
-
 const userUrl: UserUrlDto = {
-  fullUrl: 'https://youtube.com',
+  fullUrl: 'https://github.com/obkin',
 };
+
+const urlHash = 'zw1ous';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
@@ -22,13 +22,26 @@ describe('AppController (e2e)', () => {
     await app.init();
   });
 
-  it('/shrinker (POST) - success', () => {
-    return request(app.getHttpServer()).post('/shrinker').send(userUrl).expect(201);
+  it('/shrinker (POST) - success', async () => {
+    return request(app.getHttpServer())
+      .post('/shrinker')
+      .send(userUrl)
+      .expect(201)
+      .then((res: request.Response) => {
+        expect(res).toBeDefined();
+      });
   });
 
-  it('/shrinker/:hash (GET) - success', () => {
+  it('/shrinker/:hash (GET) - success (redirect to correct url)', () => {
     return request(app.getHttpServer()).get(`/shrinker/:${urlHash}`).expect(301);
   });
+
+  // it('/shrinker/:hash (GET) - fail (redirect to 404)', () => {
+  //   return request(app.getHttpServer())
+  //     .get(`/shrinker/:${urlHash}`)
+  //     .expect(301)
+  //     .expect('https://en.wikipedia.org/wiki/HTTP_404');
+  // });
 
   afterEach(async () => {
     await app.close();
